d6438593904029b453e87f23e29674ad455f3ca2
jhon11390 <meljhon2@gmail.com>
2020-04-01 13:23:32 -0500

    Se crea contructor para la aplicacion contador de caracteres

--- src/App.js
+++ src/App.js
@@ -2,13 +2,25 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      words: ''
+    }
+  }
   render() {
     return (
       <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
+        <textarea rows="3" value={this.state.words} onChange={this.updateWords.bind(this)}></textarea>
+        <div className="counter">{this.state.words.length}</div>
       </div>
     );
+  } 
+  updateWords(event){
+    this.setState({
+      words: event.target.value
+    });
   }
 }
 
